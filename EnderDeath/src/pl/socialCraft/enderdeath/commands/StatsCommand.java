292fba3b26fac3a24be31816b2b7e44aa8b008bc
Piotr Brzozowski <stirante@interia.eu>
2012-11-24 19:40:22 +0100

    New commands

---
+++ EnderDeath/src/pl/socialCraft/enderdeath/commands/StatsCommand.java
@@ -0,0 +1,21 @@
+package pl.socialCraft.enderdeath.commands;
+
+import org.bukkit.entity.Player;
+
+import pl.socialCraft.enderdeath.Config;
+import pl.socialCraft.enderdeath.EnderDeath;
+
+public class StatsCommand extends Command{
+
+	@Override
+	public void performCommand(Player sender, String cmd, String[] args) {
+		sender.sendMessage(EnderDeath.getRound().getFormattedScore()[0]);
+		sender.sendMessage(EnderDeath.getRound().getFormattedScore()[1]);
+	}
+
+	@Override
+	public String getLabel() {
+		return Config.getCommand("stats");
+	}
+	
+}
---
+++ EnderDeath/src/pl/socialCraft/enderdeath/commands/TimeCommand.java
@@ -0,0 +1,25 @@
+package pl.socialCraft.enderdeath.commands;
+
+import org.bukkit.entity.Player;
+
+import pl.socialCraft.enderdeath.Config;
+import pl.socialCraft.enderdeath.EnderDeath;
+
+public class TimeCommand extends Command {
+	
+	@Override
+	public void performCommand(Player sender, String cmd, String[] args) {
+		if (EnderDeath.getRound().getPlayerTeam(sender) != null)
+			sender.sendMessage(Config.getMessage("time", formatTime(EnderDeath.getRound().getTime())));
+	}
+	
+	@Override
+	public String getLabel() {
+		return Config.getCommand("time");
+	}
+	
+	private String formatTime(int time){
+		return String.valueOf((int)time / 60) + ":" + String.valueOf(time % 60);
+	}
+	
+}
